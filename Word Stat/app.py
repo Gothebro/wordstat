3d5ab3a22c5675f00bd4fbe83cc25ba01f76738c
Griffin Owens <Gothebro@gmail.com>
2024-02-09 09:31:56 -0600

    started app

---
+++ Word Stat/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("index.html")
+
+
+#
+@app.route('')
+
+
+if __name__ == '__main__':
+    app.run()
